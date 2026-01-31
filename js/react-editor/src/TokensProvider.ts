979770ed611b81e79ef5a39de6334aa7fcd4d019
Henrik Cooke <henrik@cooke.se>
2026-01-31 08:51:08 +0100

    Improved highlighting

--- js/react-editor/src/TokensProvider.ts
+++ js/react-editor/src/TokensProvider.ts
@@ -1,6 +1,11 @@
 import monaco from "monaco-editor";
 import { createLexer, TokenType } from "tybscri";
 
+interface RawToken {
+  type: TokenType;
+  startIndex: number;
+}
+
 export class TybscriTokensProvider implements monaco.languages.TokensProvider {
   public getInitialState(): monaco.languages.IState {
     return new TybscriState();
@@ -10,28 +15,27 @@ export class TybscriTokensProvider implements monaco.languages.TokensProvider {
     line: string,
     state: monaco.languages.IState
   ): monaco.languages.ILineTokens {
-    // So far we ignore the state, which is not great for performance reasons
-    const tokens: monaco.languages.IToken[] = [];
-
+    // Collect all tokens first for context-aware highlighting
+    const rawTokens: RawToken[] = [];
     const lexer = createLexer(line);
-    // lexer.removeErrorListeners();
-    // lexer.addErrorListener({
-    //   syntaxError: (
-    //     _recognizer: any,
-    //     _offendingSymbol: any,
-    //     _line: number,
-    //     charPositionInLine: number
-    //   ) => tokens.push({ scopes: "invalid", startIndex: charPositionInLine }),
-    // });
 
     while (lexer.tokenType !== TokenType.EOF) {
-      const myToken = {
-        scopes: scopeMap[lexer.tokenType] || "default",
+      rawTokens.push({
+        type: lexer.tokenType,
         startIndex: lexer.column - 1,
-      };
-      tokens.push(myToken);
+      });
       lexer.advance();
     }
+
+    // Map tokens to scopes with context awareness
+    const tokens: monaco.languages.IToken[] = rawTokens.map((token, i) => {
+      const prev = rawTokens[i - 1];
+      const next = rawTokens[i + 1];
+      return {
+        startIndex: token.startIndex,
+        scopes: getScopeForToken(token.type, prev?.type, next?.type),
+      };
+    });
 
     return { tokens: tokens, endState: new TybscriState() };
   }
@@ -53,11 +57,74 @@ function mapToScope(tokenTypes: TokenType[], scope: string) {
   return tokenTypes.reduce((agg, c) => (agg[c] = scope) && agg, {} as any);
 }
 
-const scopeMap: { [key in TokenType]: string } = {
+const scopeMap: { [key in TokenType]?: string } = {
+  // Keywords (control flow)
   ...mapToScope(
-    [L.IF, L.VAR, L.VAL, L.TRUE, L.FALSE, L.ELSE, L.FUN, L.IS, L.RETURN],
+    [L.IF, L.ELSE, L.FUN, L.VAR, L.VAL, L.RETURN, L.IS, L.FOR, L.WHILE, L.BREAK, L.CONTINUE, L.IN],
     "keyword"
   ),
-  ...mapToScope([L.IDENTIFIER], "identifier"),
+  // Constants (true, false, null)
+  ...mapToScope([L.TRUE, L.FALSE, L.NULL], "constant.language"),
+  // Strings
   ...mapToScope([L.LINE_STRING], "string"),
+  // Numbers
+  ...mapToScope([L.INT, L.FLOAT], "number"),
+  // Operators
+  ...mapToScope(
+    [
+      L.ASSIGNMENT, L.EQEQ, L.EXCLAM_EQ, L.LT, L.GT, L.LT_EQ, L.GT_EQ,
+      L.ADD, L.SUB, L.MULT, L.DIV, L.MOD, L.AND_AND, L.OR_OR, L.OR, L.EXCLAM,
+      L.FAT_ARROW, L.INCR, L.DECR, L.QUESTION,
+    ],
+    "operator"
+  ),
+  // Punctuation
+  ...mapToScope([L.DOT, L.COMMA, L.COLON, L.SEMICOLON], "delimiter"),
+  // Brackets
+  ...mapToScope(
+    [L.LPAREN, L.RPAREN, L.LBRACKET, L.RBRACKET, L.LCURL, L.RCURL],
+    "delimiter.bracket"
+  ),
 };
+
+function getScopeForToken(
+  type: TokenType,
+  prevType: TokenType | undefined,
+  nextType: TokenType | undefined
+): string {
+  // Context-aware identifier highlighting
+  if (type === L.IDENTIFIER) {
+    // Type annotation: `x: Type` or `: Type`
+    if (prevType === (L.COLON as TokenType)) {
+      return "type";
+    }
+    // Function call: `func(`
+    if (nextType === (L.LPAREN as TokenType)) {
+      return "entity.name.function";
+    }
+    // Property access: `obj.property`
+    if (prevType === (L.DOT as TokenType)) {
+      // Could be method call or property
+      if (nextType === (L.LPAREN as TokenType)) {
+        return "entity.name.function";
+      }
+      return "variable.property";
+    }
+    // Variable definition: `var x` or `val x`
+    if (prevType === (L.VAR as TokenType) || prevType === (L.VAL as TokenType)) {
+      return "variable.definition";
+    }
+    // Function name: `fun name`
+    if (prevType === (L.FUN as TokenType)) {
+      return "entity.name.function";
+    }
+    // Type check: `is Type`
+    if (prevType === (L.IS as TokenType)) {
+      return "type";
+    }
+    // Default: variable reference
+    return "variable";
+  }
+
+  return scopeMap[type] || "default";
+}
